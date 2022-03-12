e9eee28a5c415bc029db1c6c8a2e0784e6c00bab
alsrl282 <alsrl282@naver.com>
2022-03-13 01:20:51 +0900

    removed hello.java and added performancemanager.java

---
+++ src/PerformanceManager.java
@@ -0,0 +1,20 @@
+
+public class PerformanceManager {
+	public static void main(String[] agrs) {
+		
+		System.out.println("1. Add Students");
+		System.out.println("2. Delete Students");
+		System.out.println("3. Edit Students");
+		
+		System.out.println("4. Add score");
+		System.out.println("5. Delete score");
+		System.out.println("6. Edit score");
+		
+		System.out.println("7. Add subject");
+		System.out.println("8. Delete subject");
+		System.out.println("9. Edit subject");
+		
+		System.out.println("10. Exit");
+		
+	}
+}
