cf5622e4a54b62d3ed7ed0308f0cac2700895998
alsrl282 <alsrl282@naver.com>
2022-03-21 18:04:28 +0900

    add menu selection

--- src/PerformanceManager.java
+++ src/PerformanceManager.java
@@ -1,20 +1,69 @@
+import java.util.Scanner;
 
 public class PerformanceManager {
 	public static void main(String[] agrs) {
-		
-		System.out.println("1. Add Students");
-		System.out.println("2. Delete Students");
-		System.out.println("3. Edit Students");
-		
-		System.out.println("4. Add score");
-		System.out.println("5. Delete score");
-		System.out.println("6. Edit score");
-		
-		System.out.println("7. Add subject");
-		System.out.println("8. Delete subject");
-		System.out.println("9. Edit subject");
-		
-		System.out.println("10. Exit");
-		
+		int num =0;
+		Scanner input = new Scanner(System.in);
+
+		while(num !=5){
+			System.out.println("1. Add Students");
+			System.out.println("2. Delete Students");
+			System.out.println("3. Edit Students");
+			System.out.println("4. View Students");
+			System.out.println("0. Exit");
+			System.out.println("Select one number between 0-4");
+			num= input.nextInt();
+			if (num ==1) {
+				addStudent();
+			}
+			else if (num ==2) {
+				deleteStudent();
+			}
+			else if (num ==3) {
+				editStudent();
+			}
+			else if (num ==4) {
+				viewStudent();
+			}
+			else if (num ==0) {
+				System.exit(0);
+			}
+		}
 	}
+	private static void addStudent() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Student ID:");
+		int studentId = input.nextInt();
+		System.out.print("Student name");
+		String studentName = input.next();
+		System.out.print("Email address:");
+		String studentEmail = input.next();
+		System.out.println(studentEmail);
+	}
+
+	private static void deleteStudent() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Student ID:");
+		int studentId = input.nextInt();
+	}
+
+	private static void editStudent() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Student ID:");
+		int studentId = input.nextInt();
+	}
+
+	private static void viewStudent() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Student ID:");
+		int studentId = input.nextInt();
+	}
+
 }
+
+
+
+
+
+
+
